c455e4c39f1965fb4f724f347a25a2d83e2a2a52
NOURA_ALGAHTANI <NouraHani_hotmail.com>
2023-06-14 21:36:13 +0300

    finished

--- password.py
+++ password.py
@@ -15,5 +15,36 @@ def password_strength(password):
         
         
     # to check upprercase case in Password
-    
-    
+    if re.search(r'[A-Z]', password):
+        rating += 1
+        
+    # to check lowercase cas in password
+    if re.search(r'[a-z]', password):
+        rating += 1
+
+ # Check for dig 
+    if re.search(r'\d', password):
+        rating += 1
+
+    # Check for symb
+    if re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        rating += 1
+
+    # Return the password ratings
+    if rating < 3:
+        return "weak"
+    elif rating < 5:
+        return "medium"
+    else:
+        return "strong"
+
+
+
+
+
+
+
+# here where all thr magic happens
+password = input("Enter a password: ")
+strength = password_strength(password)
+print("Password strength: " + strength)
